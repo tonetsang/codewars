5d6ac266ca48854b167c979c9745b04258af558f
Tony Tsang <offcell@gmail.com>
2021-04-04 22:52:26 -0400

    4/4/21

---
+++ CatYearsDogYears.js
@@ -0,0 +1,17 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let arr = []
+    if(humanYears === 1){
+      arr.push(humanYears)
+      arr.push(humanYears * 15)
+      arr.push(humanYears * 15)
+    }else if(humanYears === 2){
+      arr.push(humanYears)
+      arr.push(24 + humanYears * 4 - 8)
+      arr.push(24 + humanYears * 4 - 8)
+    }else if(humanYears >= 3){
+      arr.push(humanYears)
+      arr.push(24 + humanYears * 4 - 8)
+      arr.push(24 + humanYears * 5 - 10)
+    }
+    return arr
+  }
