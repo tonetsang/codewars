3993acc22d1e68f6a88071acde71e45d8bde81f4
Tony Tsang <offcell@gmail.com>
2021-04-28 22:43:44 -0400

    4/28/21

---
+++ PolishAlphabet.js
@@ -0,0 +1,3 @@
+function correctPolishLetters (string) {
+    return string.replace(/ą/g, 'a').replace(/ć/g, 'c').replace(/ę/g, 'e').replace(/ł/g, 'l').replace(/ń/g, 'n').replace(/ó/g, 'o').replace(/ś/g, 's').replace(/ź/g, 'z').replace(/ż/g, 'z')
+  }
