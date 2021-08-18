5e9719182f325682449ea79ce07f30f3b8dfb1a9
Tony Tsang <offcell@gmail.com>
2021-08-18 11:57:46 -0400

    8/18/21

---
+++ TrainingJS9LoopStatementWhileAndDoWhile.js
@@ -0,0 +1,13 @@
+function padIt(str,n){
+    let result = str
+    let i = 0
+    while (i < n) {
+      if (i % 2 === 0) {
+        result = `*${result}`
+      } else {
+        result = `${result}*`
+      }
+      i += 1
+    }
+    return result
+  }
