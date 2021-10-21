9924e3875cfeb79aa86d871a062136597303ef5b
Tony Tsang <offcell@gmail.com>
2021-10-21 12:40:05 -0400

    10/21/21

---
+++ FixYourCodeBeforeTheGardenDies.js
@@ -0,0 +1,8 @@
+function rainAmount(mm){
+    if (mm >= 40) {
+       return "Your plant has had more than enough water for today!"  
+    }
+    else {    
+      return "You need to give your plant " + (Math.abs(mm - 40)) + "mm of water"
+    }
+}
