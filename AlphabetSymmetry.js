6b99dc7cbb252f7804fc493865ec712d04c99bec
Tony Tsang <offcell@gmail.com>
2022-03-09 16:00:51 -0500

    Alphabet Symmetry

---
+++ AlphabetSymmetry.js
@@ -0,0 +1,12 @@
+function solve(arr) {
+  return arr.map(function(input) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let arr = input.toLowerCase().split('')
+    let sum = 0
+    arr.forEach(function(val, i) {
+      if(val === alphabet[i])
+        sum++
+    })
+    return sum
+  })
+}
