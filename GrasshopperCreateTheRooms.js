691ed3cbb4a6faca7511055d628612caa30b0f0d
Tony Tsang <offcell@gmail.com>
2022-08-13 12:23:09 -0400

    Grasshopper - Create the rooms

---
+++ GrasshopperCreateTheRooms.js
@@ -0,0 +1,7 @@
+// Add rooms here
+var rooms = {
+    //   Nah, I'll add them here :)
+      room1: {name:'a', description:'b', completed:'c'},
+      room2: {name:'a', description:'b', completed:'c'},
+      room3: {name:'a', description:'b', completed:'c'}
+    }
