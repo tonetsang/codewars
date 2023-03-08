981c5c93bd08a1b60c3bdc804285fe6eebba880c
Tone <offcell@gmail.com>
2023-03-08 17:40:40 -0500

    Find Your Villain Name

---
+++ FindYourVillainName.js
@@ -0,0 +1,5 @@
+function getVillainName(birthday){
+  const m = ["Evil","Vile","Cruel","Trashy","Despicable","Embarrassing","Disreputable","Atrocious","Twirling","Orange","Terrifying","Awkward"];
+  const d = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"];
+  return `The ${m[birthday.getMonth()]} ${d[birthday.getDate() % 10]}`
+}
