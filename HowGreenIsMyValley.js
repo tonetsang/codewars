4d0b19eb22aa69c36bafba69f833de29bde1946c
Tone <offcell@gmail.com>
2023-05-25 18:39:18 -0400

    How Green Is My Valley?

---
+++ HowGreenIsMyValley.js
@@ -0,0 +1,7 @@
+function makeValley(arr) {
+    var leftWing = []
+    var rightWing = []
+    arr.sort((a, b) => b - a)
+      .forEach((el, i) => i % 2 == 0 ? leftWing.push(el) : rightWing.unshift(el))  
+    return [...leftWing, ...rightWing]
+}
