65b65a204242308e9e5dcceb7863693db6f3952a
Tone <offcell@gmail.com>
2023-08-20 16:51:05 -0400

    Selective fear of numbers

---
+++ SelectiveFearOfNumbers.js
@@ -0,0 +1,9 @@
+var AmIAfraid = function(day, num){
+  return day == 'Monday'&& num == 12 || 
+    day == 'Tuesday' && num > 95 || 
+    day == 'Wednesday' && num == 34 || 
+    day == 'Thursday' && num == 0 || 
+    day == 'Friday' && num % 2 == 0 || 
+    day == 'Saturday' && num == 56 ||
+    day == 'Sunday' && Math.abs(num) == 666 
+}
