8be99cbfcb0b204c676496b49abac1ce37834208
Tone <offcell@gmail.com>
2023-12-22 17:40:41 -0500

    Functions of Integers on Cartesian Plane

---
+++ FunctionsOfIntegersOnCartesianPlane.js
@@ -0,0 +1,9 @@
+function sumin(n) {
+    return n * (n + 1) * (2 * n + 1) / 6
+  }
+  function sumax(n) {
+    return n * (n + 1) * (4 * n - 1) / 6
+  }
+  function sumsum(n) {
+    return n * n * (n + 1)
+  }
