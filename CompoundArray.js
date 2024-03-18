35b0f68e30ba5ac64b0ab4e9c5ecf6e30766073d
Tone <offcell@gmail.com>
2024-03-18 14:57:22 -0400

    CompoundArray

---
+++ CompoundArray.js
@@ -0,0 +1,8 @@
+function compoundArray(a, b) {
+    let arr = []
+    for(let i = 0; i < (a.length > b.length ? a.length : b.length); i++){
+        arr.push(a[i])
+        arr.push(b[i]) 
+    }
+    return arr.filter(x => x != undefined)
+  }
