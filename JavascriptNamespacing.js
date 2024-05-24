94d4d11e3a5e064e9f2f6008e289d9b68840492c
Tone <offcell@gmail.com>
2024-05-24 16:29:15 -0400

    Javascript Namespacing

---
+++ JavascriptNamespacing.js
@@ -0,0 +1,10 @@
+// MyNamespace is provided. Define MyClass ...
+MyNamespace.MyClass = class{
+    constructor(hello){
+      this.hello = hello
+    }
+    
+    sayHello(){
+      return this.hello
+    }
+  }
